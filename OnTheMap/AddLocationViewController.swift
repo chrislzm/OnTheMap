324b7fece3687ffc8293f60ad332d3ca89d8012b
chrislzm <me@chrisleung.com>
2017-04-30 16:39:30 -0700

    Added text input validation methods and alerts

--- OnTheMap/AddLocationViewController.swift
+++ OnTheMap/AddLocationViewController.swift
@@ -6,11 +6,37 @@
 //  Copyright © 2017 Chris Leung. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 class AddLocationViewController:UIViewController {
     
+    @IBOutlet weak var locationTextView: UITextField!
+    @IBOutlet weak var websiteTextView: UITextField!
+    
+    @IBAction func findLocationButtonPressed(_ sender: Any) {
+        if locationTextView.text!.isEmpty {
+            displayAlertWithOKButton("Location Is Empty","Please enter a location")
+        } else if websiteTextView.text!.isEmpty {
+            displayAlertWithOKButton("Website Is Empty","Please enter a website")
+        } else if !validUrl(urlString: websiteTextView.text!) {
+            displayAlertWithOKButton("Invalid Website","Please enter a valid website address beginning with http(s)://")
+        }
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func validUrl (urlString: String?) -> Bool {
+        //Check for nil
+        if let urlString = urlString {
+            // create NSURL instance
+            if let url = URL(string: urlString) {
+                // check if your application can open the NSURL instance
+                return UIApplication.shared.canOpenURL(url)
+            }
+        }
+        return false
     }
 }
