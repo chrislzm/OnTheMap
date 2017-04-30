29a8adcd86f8c8300dc1dd5ce4351c895011f06e
chrislzm <me@chrisleung.com>
2017-04-29 22:37:01 -0700

    Added the "AddLocationViewController"

---
+++ OnTheMap/AddLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Chris Leung on 4/29/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController:UIViewController {
+    
+}
