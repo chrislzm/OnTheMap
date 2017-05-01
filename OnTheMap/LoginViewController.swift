499eddbbbe5c5491dd049580362b798d95fb6109
chrislzm <me@chrisleung.com>
2017-04-30 21:55:34 -0700

    Facebook login is returning the correct status! Need to wire everything together now

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -42,6 +42,19 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
+    @IBAction func loginWithFacebookPressed(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+            }
+        }
+    }
     
     // MARK: Lifecycle
     
